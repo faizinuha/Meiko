8341b96478c74513fa743c8f1d3c73808013f0d3
Mardev <rozakadm@gmail.com>
2024-11-17 19:48:33 +0700

    Delete 2Pages

--- cli/info-solid.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-
-const version = "2.0.0";
-console.log(`Code-Info CLI - Version ${version}`);
-
-// Tambahkan logika CLI lainnya di sini
-console.log("Here's some information about the CLI.");
